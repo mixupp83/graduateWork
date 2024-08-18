996e00dae6eb68f363864832083ab94aa5f4f564
verei <verein83@mail.ru>
2024-08-18 15:31:53 +0400

    18/08 Добавлены файлы к проекту диплома на Django

--- graduate_django/graduate_project/myapp/models.py
+++ graduate_django/graduate_project/myapp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-class Item(models.Model):
-    name = models.CharField(max_length=100)
+class Task(models.Model):
+    title = models.CharField(max_length=200)
     description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- graduate_django/graduate_project/myapp/urls.py
+++ graduate_django/graduate_project/myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.task_list, name='task_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/new/', views.task_new, name='task_new'),
+    path('task/<int:pk>/edit/', views.task_edit, name='task_edit'),
 ]
