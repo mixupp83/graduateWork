b13454c0f31fd50a9813bcaed538937227e2b132
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-08-29 15:33:17 +0400

    28/08 Изменения в проекте. fastapi.

--- graduate_fastapi/app/main.py
+++ graduate_fastapi/app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import SessionLocal, engine
@@ -7,7 +9,8 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
+templates = Jinja2Templates(directory="app/templates")
+
 def get_db():
     db = SessionLocal()
     try:
@@ -15,43 +18,14 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/books/", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
-    db_book = models.Book(**book.dict())
-    db.add(db_book)
-    db.commit()
-    db.refresh(db_book)
-    return db_book
-
-@app.get("/books/", response_model=list[schemas.Book])
-def read_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+@app.get("/books/", response_class=HTMLResponse)
+def read_books(request: Request, skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     books = db.query(models.Book).offset(skip).limit(limit).all()
-    return books
+    return templates.TemplateResponse("books.html", {"request": request, "books": books})
 
-@app.get("/books/{book_id}", response_model=schemas.Book)
-def read_book(book_id: int, db: Session = Depends(get_db)):
-    db_book = db.query(models.Book).filter(models.Book.id == book_id).first()
-    if db_book is None:
+@app.get("/books/{book_id}", response_class=HTMLResponse)
+def read_book(request: Request, book_id: int, db: Session = Depends(get_db)):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if book is None:
         raise HTTPException(status_code=404, detail="Book not found")
-    return db_book
-
-@app.put("/books/{book_id}", response_model=schemas.Book)
-def update_book(book_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
-    db_book = db.query(models.Book).filter(models.Book.id == book_id).first()
-    if db_book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    for var, value in vars(book).items():
-        setattr(db_book, var, value)
-    db.add(db_book)
-    db.commit()
-    db.refresh(db_book)
-    return db_book
-
-@app.delete("/books/{book_id}", response_model=schemas.Book)
-def delete_book(book_id: int, db: Session = Depends(get_db)):
-    db_book = db.query(models.Book).filter(models.Book.id == book_id).first()
-    if db_book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    db.delete(db_book)
-    db.commit()
-    return db_book
+    return templates.TemplateResponse("books.html", {"request": request, "books": [book]})
